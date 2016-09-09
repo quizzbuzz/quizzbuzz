a104aa8a03713184bf283c117863e4793284fe3b
Jack Oddy <jack.oddy91@gmail.com>
2016-09-09 16:49:43 +0100

    adds a shuffle to the questions and their options

--- test/support/factory.ex
+++ test/support/factory.ex
@@ -8,11 +8,17 @@ defmodule Quizzbuzz.Factory do
     %Game{
       title: "Game 1",
       topic: "Philosophy",
-      questions: [%Question{
-      body: "How many roads must a man walk down before he is a man?",
-      options: ["1","2","3","4"],
-      answer: "1",
-    }]
+      questions: [
+        %Question{
+          body: "How many roads must a man walk down before he is a man?",
+          options: ["1","2","3","4"],
+          answer: "1",
+        },
+        %Question{
+          body: "How much wood would a woodchuck chuck if a woodchuck could chuck wood?",
+          options: ["A lot", "Some", "Not much", "None"],
+          answer: "A lot",
+          }]
     }
   end
 
