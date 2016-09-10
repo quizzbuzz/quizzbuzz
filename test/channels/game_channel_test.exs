dfd5c29e22491a99d582bf688d52a68183f171b9
Rebecca Pearce <becciepearce@hotmail.co.uk>
2016-09-10 17:58:30 +0100

    test of sockets updated

--- test/channels/game_channel_test.exs
+++ test/channels/game_channel_test.exs
@@ -1,21 +1,32 @@
 defmodule Quizzbuzz.GameChannelTest do
   use Quizzbuzz.ChannelCase
+
+  import Quizzbuzz.Factory
+
   alias Quizzbuzz.GameChannel
 
   setup do
+
     insert(:game)
-    {:ok, game_info, socket} =
-      socket("user_id", %{some: :assign})
+    {:ok, game, socket} =
+      socket("game", %{some: :assign})
       |> subscribe_and_join(GameChannel, "game:lobby")
 
-      IO.inspect game_info
-
-    {:ok, game_info: game_info, socket: socket}
+    {:ok, game_info: game, socket: socket}
   end
 
   test "game info is returned on join of the lobby" do
 
   end
+
+  test "new question is returned on the reveival of an answer", %{socket: socket} do
+    push socket, "answer", %{"answer" => "1"}
+    assert_push "new_question", %{question:
+                                  %{answer: "1",
+                                  body: "How many roads must a man walk down before he is a man?",
+                                  options: [_, _, _, _]}}
+  end
+
 
   test "ping replies with status ok", %{socket: socket} do
     ref = push socket, "ping", %{"hello" => "there"}
