6763c5aaddec531af73944e87654cab8eced1283
Rebecca Pearce <becciepearce@hotmail.co.uk>
2016-09-12 15:09:39 +0100

    adds questions to the DB seed

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -34,7 +34,46 @@ Quizzbuzz.Repo.insert!(%Quizzbuzz.Game{
         options: ["an orange","a dog","a man","a cat"],
         answer: "a man",
       }
-
+      %Quizzbuzz.Question{
+        body: "Which of these is a mammal",
+        options: ["an orange","a dolphin","a blobfish","a caterpiller"],
+        answer: "a dolphin",
+      }
+      %Quizzbuzz.Question{
+        body: "What type of language is Romanian",
+        options: ["Romance","Slavic","Germanic","Mr. Bombastic"],
+        answer: "Romance",
+      }
+      %Quizzbuzz.Question{
+        body: "What is a ratus ratus",
+        options: ["rat","pokemon","type of coffee","coding language"],
+        answer: "rat",
+      }
+      %Quizzbuzz.Question{
+        body: "Which pokemon is the first in the origin 150",
+        options: ["Bulbasaur","Pikachu","Charmander","Ditto"],
+        answer: "Bulbasaur",
+      }
+      %Quizzbuzz.Question{
+        body: "Never gonna...",
+        options: ["give you up","let you down","hold you down","desert you"],
+        answer: "give you up",
+      }
+      %Quizzbuzz.Question{
+        body: "Where is the ponte dei sospiri",
+        options: ["Venice","Rome","Reykjavik","Madrid"],
+        answer: "Venice",
+      }
+      %Quizzbuzz.Question{
+        body: "Who wrote Catch-22",
+        options: ["Mozart","George Orwell","J.K Rowling","Joseph Heller"],
+        answer: "Joseph Heller",
+      }
+      %Quizzbuzz.Question{
+        body: "What year was the NHS formed",
+        options: ["1947","1948","1949","1950"],
+        answer: "1948",
+      }
 
     ]
   })
