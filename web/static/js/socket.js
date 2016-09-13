126e1de8c5399ca823ef20cc08bf80c297611f55
Aga Kowalczuk <kowalczuk.a@outlook.com>
2016-09-13 14:00:19 +0100

    run testing fix

--- web/static/js/socket.js
+++ web/static/js/socket.js
@@ -4,8 +4,11 @@
 // To use Phoenix channels, the first step is to import Socket
 // and connect at the socket path in "lib/my_app/endpoint.ex":
 import { Socket } from "phoenix"
-const token = document.getElementsByTagName('meta')[0].getAttribute('content')
-const socket = new Socket("/socket", {params: {token: token}})
+if (document.getElementsByTagName('meta')[0]) {
+  const token = document.getElementsByTagName('meta')[0].getAttribute('content')
+  const socket = new Socket("/socket", {params: {token: token}})
+}
+const socket = new Socket("/socket")
 
 // When you connect, you'll often need to authenticate the client.
 // For example, imagine you have an authentication plug, `MyAuth`,
