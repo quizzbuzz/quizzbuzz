7599c35331263429eb9fb617c02e8ec1fae782d0
Sophie Gill <sophie-gill@hotmail.com>
2016-09-14 12:00:39 +0100

    adds additional questions

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -1,62 +1,237 @@
 
 
 questions = [%Quizzbuzz.Question{
-        body: "What is 1+1?",
-        options: ["1","2","3","4"],
-        answer: "2"
+        body: "What is the capital city of Jamaica?",
+        options: ["Kingston","George Town","Kingstown","Road Town"],
+        answer: "Kingston"
       },
       %Quizzbuzz.Question{
-        body: "Are we awesome?",
-        options: ["Yes","No","Maybe","Yellow"],
-        answer: "Yes",
+        body: "What is the sum of the internal angles o a triangle?",
+        options: ["80 degrees","90 degress","180 degrees","360 degrees"],
+        answer: "180 degrees"
       },
       %Quizzbuzz.Question{
-        body: "How much wood would a woodchuck chuck if a woodchuck could chuck wood?",
-        options: ["some","a lot","a bit","none"],
-        answer: "a lot",
+        body: "Who directed The Matrix?",
+        options: ["James Cameron","Christopher Nolan","Alex Proyas","Wachowski brothers"],
+        answer: "Wachowski brothers"
       },
       %Quizzbuzz.Question{
-        body: "What has four legs at dawn, two at midday and three at sunset?",
-        options: ["an orange","a dog","a man","a cat"],
-        answer: "a man",
+        body: "Which philosopher wrote 'On Liberty'?",
+        options: ["Friedrich Nietzsche","John Stuart Mill","David Hume","Karl Marx"],
+        answer: "John Stuart Mill"
       },
       %Quizzbuzz.Question{
-        body: "Which of these is a mammal",
-        options: ["an orange","a dolphin","a blobfish","a caterpiller"],
-        answer: "a dolphin",
+        body: "What is the traditional gift for a 5th wedding anniversary?",
+        options: ["Wood","Copper","Sugar","Flowers"],
+        answer: "Wood"
       },
       %Quizzbuzz.Question{
-        body: "What type of language is Romanian",
-        options: ["Romance","Slavic","Germanic","Mr. Bombastic"],
-        answer: "Romance",
+        body: "What type of language is Romanian?",
+        options: ["Romance","Slavic","Germanic","Turkic"],
+        answer: "Romance"
       },
       %Quizzbuzz.Question{
-        body: "What is a ratus ratus",
-        options: ["rat","pokemon","type of coffee","coding language"],
-        answer: "rat",
+        body: "What was the name of Adam and Eve's first child?",
+        options: ["Cain","Joseph","Elijah","Jacob"],
+        answer: "Cain"
       },
       %Quizzbuzz.Question{
-        body: "Which pokemon is the first in the original 150",
-        options: ["Bulbasaur","Pikachu","Charmander","Ditto"],
-        answer: "Bulbasaur",
+        body: "In Greek mythology, who was the goddess of the hunt?",
+        options: ["Artemis","Hera","Hestia","Demeter"],
+        answer: "Artemis"
       },
       %Quizzbuzz.Question{
-        body: "Never gonna...",
-        options: ["give you up","let you down","hold you down","desert you"],
-        answer: "give you up",
+        body: "In which American state is the Pentagon?",
+        options: ["Virginia","Maryland","Washington","Pennsylvania"],
+        answer: "Virginia"
       },
       %Quizzbuzz.Question{
-        body: "Where is the ponte dei sospiri",
-        options: ["Venice","Rome","Reykjavik","Madrid"],
-        answer: "Venice",
+        body: "Where is the Ponte dei Sospiri?",
+        options: ["Venice","Rome","Naples","Milan"],
+        answer: "Venice"
       },
       %Quizzbuzz.Question{
-        body: "Who wrote Catch-22",
-        options: ["Mozart","George Orwell","J.K Rowling","Joseph Heller"],
-        answer: "Joseph Heller",
+        body: "Who wrote the novel Catch-22?",
+        options: ["Ernest Hemingway","George Orwell","J.K Rowling","Joseph Heller"],
+        answer: "Joseph Heller"
       },
       %Quizzbuzz.Question{
-        body: "What year was the NHS formed",
+        body: "What is the medical name for the shoulder blade?",
+        options: ["Scapula","Humerus","Patella","Clavicle"],
+        answer: "Scapula"
+      },
+      %Quizzbuzz.Question{
+        body: "What does a Cartographer make?",
+        options: ["Maps","Watches","Bags","Wheels"],
+        answer: "Maps"
+      },
+      %Quizzbuzz.Question{
+        body: "Who wrote the play 'Look Back in Anger'?",
+        options: ["John Osbourne","Harold Pinter","Caryl Churchill","Sarah Kane"],
+        answer: "John Osbourne",
+      },
+      %Quizzbuzz.Question{
+        body: "In which county is the royal residence Sandringham?",
+        options: ["Norfolk","Suffolk","Kent","Cambridgeshire"],
+        answer: "Norfolk"
+      },
+      %Quizzbuzz.Question{
+        body: "Lent Lilly is another name for what common flower?",
+        options: ["Daffodil","Tulip","Snowdrop","Bluebell"],
+        answer: "Daffodil"
+      },
+      %Quizzbuzz.Question{
+        body: "In which city was Mozart born?",
+        options: ["Salzburg","Vienna","Innsbruck","Graz"],
+        answer: "Salzburg"
+      },
+      %Quizzbuzz.Question{
+        body: "Which organ of the body produces the enzyme pepsin?",
+        options: ["Stomach","Liver","Pancreas","Small intestine"],
+        answer: "Stomach"
+      },
+      %Quizzbuzz.Question{
+        body: "Titan is the largest moon of what planet?",
+        options: ["Saturn","Jupiter","Uranus","Neptune"],
+        answer: "Saturn",
+      },
+      %Quizzbuzz.Question{
+        body: "Which international rugby team are known as The Pumas?",
+        options: ["Australia","Argentina","New Zealand","Japan"],
+        answer: "Argentina"
+      },
+      %Quizzbuzz.Question{
+        body: "Cob and Pen and the male and female names for which bird?",
+        options: ["Duck","Goose","Swan","Penguin"],
+        answer: "Swan"
+      },
+      %Quizzbuzz.Question{
+        body: "What song was the Beatles first US number 1?",
+        options: ["I Want to Hold Your Hand","Can't Buy Me Love","A Hard Day's Night","Help!"],
+        answer: "I Want to Hold Your Hand"
+      },
+      %Quizzbuzz.Question{
+        body: "In what year whast One Canada Square completed?",
+        options: ["1990","1991","1992","1989"],
+        answer: "1991"
+      },
+      %Quizzbuzz.Question{
+        body: "Which sea is bordered by the Dalmation coast?",
+        options: ["Mediterranean","Adriatic","Aegean","Baltic"],
+        answer: "Adriatic"
+      },
+      %Quizzbuzz.Question{
+        body: "In what year was the first full-length episode of The Simpsons broadcast?",
+        options: ["1989","1990","1991","1992"],
+        answer: "1989"
+      },
+      %Quizzbuzz.Question{
+        body: "What many stars are there on the flag of China?",
+        options: ["5","6","7","9"],
+        answer: "5"
+      },
+      %Quizzbuzz.Question{
+        body: "What is the name of Buffy the Vampire Slayer's school?",
+        options: ["Sunnydale High","Bates High","Sweet Valley High","Rydell High"],
+        answer: "Sunnydale High"
+      },
+      %Quizzbuzz.Question{
+        body: "Which tube line has the most stations?",
+        options: ["Central Line","Picadilly Line","District Line","Circle Line"],
+        answer: "Distric Line"
+      },
+      %Quizzbuzz.Question{
+        body: "Ring-tailed Lemurs are native to which country?",
+        options: ["Madagascar","Borneo","Cameroon","Indonesia"],
+        answer: "Madagascar"
+      },
+      %Quizzbuzz.Question{
+        body: "Guy Garvey is the lead singer of which band?",
+        options: ["Elbow","Coldplay","Snow Patrol","The National"],
+        answer: "Elbow"
+      },
+      %Quizzbuzz.Question{
+        body: "Mr Knightley is a character in which Jane Austen novel?",
+        options: ["Emma","Mansfield Park","Persuasion","Northanger Abbey"],
+        answer: "Emma"
+      },
+      %Quizzbuzz.Question{
+        body: "What is the home stadium of Scottish football team Rangers FC?",
+        options: ["Ibrox","Celtic Park","Hampden Park","Pittodrie"],
+        answer: "Ibrox"
+      },
+      %Quizzbuzz.Question{
+        body: "In the Mr Men series, what colour is Mr Tickle?",
+        options: ["Blue","Purple","Green","Orange"],
+        answer: "Orange"
+      },
+      %Quizzbuzz.Question{
+        body: "In which city can you visit the Anne Frank house?",
+        options: ["Berlin","Paris","Amsterdam","Brussels"],
+        answer: "Amsterdam"
+      },
+      %Quizzbuzz.Question{
+        body: "Who was the first femal artist to have a self-penned UK number one?",
+        options: ["Kate Bush","Madonna","Sinead O'Connor","Diana Ross"],
+        answer: "Kate Bush"
+      },
+      %Quizzbuzz.Question{
+        body: "On what date is International Women's Day?",
+        options: ["February 8th","February 20th","March 8th","April 20th"],
+        answer: "March 8th"
+      },
+      %Quizzbuzz.Question{
+        body: "Who was the first man to walk on the surface of the moon?",
+        options: ["Neil Armstrong","John Glenn","Alan Bean","Buzz Aldrin"],
+        answer: "Neil Armstrong"
+      },
+      %Quizzbuzz.Question{
+        body: "What metal is the main component of Pewter?",
+        options: ["Tin","Iron","Aluminium","Mercury"],
+        answer: "Tin"
+      },
+      %Quizzbuzz.Question{
+        body: "Who starred opposite Patrick Swayze in Dirty Dancing?",
+        options: ["Jennifer Grey","Molly Ringwald","Demi Moore","Jennifer Connelly"],
+        answer: "Jennifer Grey"
+      },
+      %Quizzbuzz.Question{
+        body: "Zucchini is an alternative name for which vegetable?",
+        options: ["Pepper","Aubergine","Courgette","Sweetcorn"],
+        answer: "Courgette"
+      },
+      %Quizzbuzz.Question{
+        body: "In what year did the 'gunpowder plot' take place?",
+        options: ["1605","1620","1615","1630"],
+        answer: "1605"
+      },
+      %Quizzbuzz.Question{
+        body: "In which sport would you use a foil and sabre?",
+        options: ["Archery","Fencing","Horse riding","Shooting"],
+        answer: "Fencing"
+      },
+      %Quizzbuzz.Question{
+        body: "Mars was the Roman god of what?",
+        options: ["War","Love","Farming","Health"],
+        answer: "War"
+      },
+      %Quizzbuzz.Question{
+        body: "In what year did Blind Date first air?",
+        options: ["1982","1983","1984","1985"],
+        answer: "1985"
+      },
+      %Quizzbuzz.Question{
+        body: "Which bird has the largest wing-span of any living bird?",
+        options: ["Golden Eagle","Albatross","California Condor","Great White Pelican"],
+        answer: "Albatross"
+      },
+      %Quizzbuzz.Question{
+        body: "In which sport do women compete for the Uber cup?",
+        options: ["Tennis","Squash","Badminton","Table Tennis"],
+        answer: "Badminton"
+      },
+      %Quizzbuzz.Question{
+        body: "What year was the NHS formed?",
         options: ["1947","1948","1949","1950"],
         answer: "1948"
       }]
@@ -65,68 +240,81 @@ Enum.map(questions, &(Quizzbuzz.Repo.insert!(&1)))
 
 users = [%Quizzbuzz.User{
     email: "wawa@email.com",
+    username: "wawa",
     password_hash: "rtyuiopjihgjhcfvnbmnjlhuiyut",
-    high_score: 192
+    high_score: 92
   },
   %Quizzbuzz.User{
     email: "solo@email.com",
+    username: "solo",
     password_hash: "rtyuiopjihgjhcfvnbmnjlhuiyut",
-    high_score: 1222
+    high_score: 22
   },
   %Quizzbuzz.User{
     email: "didi@email.com",
+    username: "didi",
     password_hash: "rtyuiopjihgjhcfvnbmnjlhuiyut",
-    high_score: 373
+    high_score: 73
   },
   %Quizzbuzz.User{
     email: "dada@email.com",
+    username: "dada",
     password_hash: "rtyuiopjihgjhcfvnbmnjlhuiyut",
-    high_score: 123
+    high_score: 82
   },
   %Quizzbuzz.User{
     email: "lolo@email.com",
+    username: "lolo",
     password_hash: "rtyuiopjihgjhcfvnbmnjlhuiyut",
-    high_score: 568
+    high_score: 68
   },
   %Quizzbuzz.User{
     email: "mama@email.com",
+    username: "mama",
     password_hash: "rtyuiopjihgjhcfvnbmnjlhuiyut",
-    high_score: 654
+    high_score: 65
   },
   %Quizzbuzz.User{
     email: "momo@email.com",
+    username: "momo",
     password_hash: "rtyuiopjihgjhcfvnbmnjlhuiyut",
-    high_score: 789
+    high_score: 78
   },
   %Quizzbuzz.User{
     email: "kaka@email.com",
+    username: "kaka",
     password_hash: "rtyuiopjihgjhcfvnbmnjlhuiyut",
-    high_score: 194
+    high_score: 19
   },
   %Quizzbuzz.User{
     email: "haha@email.com",
+    username: "haha",
     password_hash: "rtyuiopjihgjhcfvnbmnjlhuiyut",
-    high_score: 194
+    high_score: 80
   },
   %Quizzbuzz.User{
     email: "yaya@email.com",
+    username: "yaya",
     password_hash: "rtyuiopjihgjhcfvnbmnjlhuiyut",
-    high_score: 267
+    high_score: 67
   },
   %Quizzbuzz.User{
     email: "lili@email.com",
+    username: "lili",
     password_hash: "rtyuiopjihgjhcfvnbmnjlhuiyut",
-    high_score: 111
+    high_score: 45
   },
   %Quizzbuzz.User{
     email: "fofo@email.com",
+    username: "fofo",
     password_hash: "rtyuiopjihgjhcfvnbmnjlhuiyut",
-    high_score: 1298
+    high_score: 29
   },
   %Quizzbuzz.User{
     email: "zozo@email.com",
+    username: "zozo",
     password_hash: "rtyuiopjihgjhcfvnbmnjlhuiyut",
-    high_score: 766
+    high_score: 76
   }]
 
 Enum.map(users, &(Quizzbuzz.Repo.insert!(&1)))
