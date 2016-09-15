0faa4762b1b3857e00eef420c2e179b0cd1190a4
Jack Oddy <jack.oddy91@gmail.com>
2016-09-15 12:26:51 +0100

    refactors further into separate modules

--- web/channels/game_server.ex
+++ web/channels/game_server.ex
@@ -1,42 +1,37 @@
-defmodule Game.Server do
+defmodule Quizzbuzz.Game.Server do
   use GenServer
 
-  def start(name) do
-    GenServer.start_link(__MODULE__, [], name: name)
-  end
-
-  def end_game(servers) do
-    Enum.each servers, &(GenServer.stop(&1))
+  def start(name, questions \\ []) do
+    try do
+    GenServer.start_link(__MODULE__, questions, name: name)
+    catch
+      {:error, _} -> {:ok, "game already started"}
+    end
   end
 
   def add_to_queue(name, payload, socket, game_size) do
     GenServer.call(name, {:wait, payload, socket, game_size})
   end
 
-  def start_new_game(game_id, questions) do
-    GenServer.start(__MODULE__, questions, name: game_id)
-  end
-
   def pop(name) do
     GenServer.call(name, :pop)
   end
 
-    def handle_call(:pop, _from, [h | t]) do
-      {:reply, h, t}
-    end
+  def handle_call(:pop, _from, [h | t]) do
+    {:reply, h, t}
+  end
 
-    def handle_call(:pop, _from, []) do
-      {:reply,:end_game, []}
-    end
+  def handle_call(:pop, _from, []) do
+    {:reply,:end_game, []}
+  end
 
-    def handle_call({:wait, payload, socket, game_size}, _from, queue) do
-      player = %{socket: socket, payload: payload}
-      players = [player | queue]
-      if length(players) == game_size do
-        {:reply, players, []}
-      else
-        {:reply, :wait, players}
-      end
+  def handle_call({:wait, payload, socket, game_size}, _from, queue) do
+    player = %{socket: socket, payload: payload}
+    players = [player | queue]
+    if length(players) == game_size do
+      {:reply, players, []}
+    else
+      {:reply, :wait, players}
     end
-
+  end
 end
---
+++ web/channels/one_player_channel.ex
@@ -0,0 +1,55 @@
+defmodule Quizzbuzz.OnePlayerChannel do
+  use Quizzbuzz.Web, :channel
+  import Quizzbuzz.GameHelper
+
+  alias Quizzbuzz.Game.Server
+  alias Phoenix.Socket
+
+  @game_size 1
+  @room_prefix "one_player:"
+
+  def join(@room_prefix <> game_id, payload, socket) do
+    start_new_game(game_id)
+    {:ok, Socket.assign(socket, :game_id, game_id) }
+  end
+
+  def handle_in("ready", payload, socket) do
+    case handle_answer(payload, socket, @game_size) do
+      :queue_not_full -> push socket, "waiting", %{}
+      {:end_game, players} -> report_results(players)
+      {:question, question} -> broadcast! socket, "new_question", question
+    end
+    {:noreply, socket}
+  end
+
+  def handle_in("answer", payload, socket) do
+    case handle_answer(payload, socket, @game_size) do
+      :queue_not_full -> push socket, "waiting", %{}
+      {:end_game, players} -> report_results(players)
+      {:question, question} -> broadcast! socket, "new_question", question
+    end
+    {:noreply, socket}
+  end
+
+  def handle_in("user_left", _payload, socket) do
+    broadcast! socket, "user_left", %{deserter: socket.assigns.current_user.username}
+    {:noreply, socket}
+  end
+
+  def handle_in("message", %{"body" => body}, socket) do
+    broadcast! socket, "message", %{body: body}
+    {:noreply, socket}
+  end
+
+  def terminate(_reason, socket) do
+    broadcast! socket, "user_left", %{deserter: socket.assigns.current_user.username}
+    end_game(socket)
+  end
+
+  defp report_results(players) do
+    [winner | losers] = sort_results(players)
+    push winner.socket, "end_game", %{result: "You Win", winner_score: winner.payload["score"]}
+    Enum.each(losers, &( push &1.socket, "end_game",  %{result: "You Lose", winner_score: winner.payload["score"]}))
+    update_scores(players)
+  end
+end
