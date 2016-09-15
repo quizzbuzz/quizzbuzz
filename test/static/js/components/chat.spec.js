b9d8c507af0ca49e519fcf6bc97ed6f8e5d6234f
Sophie Gill <sophie-gill@hotmail.com>
2016-09-15 11:04:09 +0100

    adjusts socket temporarily for testing

---
+++ test/static/js/components/chat.spec.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import { mount, shallow } from 'enzyme'
+import sinon from 'sinon'
+import TestUtils from 'react-addons-test-utils'
+
+import Game from '../../../../web/static/js/components/chat';
+
+describe('Chat', () => {
+  const chat = shallow(<Chat />);
+
+  console.log(chat.debug())
+
+})
--- web/static/js/socket.js
+++ web/static/js/socket.js
@@ -5,9 +5,10 @@
 // and connect at the socket path in "lib/my_app/endpoint.ex":
 import { Socket } from "phoenix"
 
-var token = document.getElementsByTagName('meta')[0].getAttribute('content')
-var socket = new Socket("/socket", {params: {token: token}})
+// var token = document.getElementsByTagName('meta')[0].getAttribute('content')
+// var socket = new Socket("/socket", {params: {token: token}})
 
+var socket = new Socket("/socket")
 
 // When you connect, you'll often need to authenticate the client.
 // For example, imagine you have an authentication plug, `MyAuth`,
