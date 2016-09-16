e61be60d6ed39c6d61b2b73a481e96a8eb39fefa
Aga Kowalczuk <kowalczuk.a@outlook.com>
2016-09-16 11:04:13 +0100

    center multi player play again button

--- web/static/js/components/multiGameover.js
+++ web/static/js/components/multiGameover.js
@@ -8,7 +8,7 @@ class Gameover extends React.Component {
         <h3 className="gameOver" id="title">Game Over!</h3>
         <h3 className="outcome" id="sub-title">{this.props.outcome}</h3>
         <h4 className="finalScore" id="sub-title">Your Score: {this.props.finalScore} / 100</h4>
-        <form action="/game">
+        <form id="play-form" action="/game">
           <button id="play" className="sizing">Play Again</button>
         </form>
       </div>
